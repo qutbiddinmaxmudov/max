def3c7c92a0b362e819ada082c9e90dc974295e6
Max <qutbiddinmaxmudov@mail.ru>
2021-12-01 22:49:54 +0500

    add mui to project and made seo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,33 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 import { IntlProvider } from 'react-intl'
-import { useRouter } from 'next/dist/client/router'
+import { useRouter } from 'next/router'
 import enLocale from '../locale/en.json'
 import ruLocale from '../locale/ru.json'
+import theme from '../src/theme'
+import createEmotionCache from '../src/theme/cache'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache: EmotionCache
+}
+
+const MyApp = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) => {
   const { locale = 'en' } = useRouter()
   return (
-    <IntlProvider locale={locale} messages={locale === 'en' ? enLocale : ruLocale}>
-      <Component {...pageProps} />
-    </IntlProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <IntlProvider locale={locale} messages={locale === 'en' ? enLocale : ruLocale}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </IntlProvider>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
---
+++ src/components/Seo.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Head } from 'next/document'
+import { useIntl } from 'react-intl'
+import { useRouter } from 'next/router'
+
+interface Props {
+  title: string
+  description: string
+  cardSize: 'summary' | 'summary_large_image'
+  imageUrl: string
+}
+
+const Seo = ({ title, description, cardSize, imageUrl }: Props) => {
+  const intl = useIntl()
+  const titleContent = intl.formatMessage({ id: title })
+  const descriptionContent = intl.formatMessage({ id: description })
+  const { locale } = useRouter()
+  const localeName = locale === 'en' ? 'en_us' : 'ru_ru'
+  return (
+    <Head>
+      <title>{titleContent}</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <meta key="twt-card" name="twitter:card" content={cardSize} data-rh="true" />
+      <meta key="twt-site" name="twitter:site" content="@Max_Mudov" data-rh="true" />
+      <meta key="og-image" property="og:image" content={imageUrl} data-rh="true" />
+      <meta key="og-url" property="og:url" content="http://www.m4x.site/" data-rh="true" />
+      <meta key="og-type" property="og:type" content="website" data-rh="true" />
+      <meta key="og-title" property="og:title" content={titleContent} data-rh="true" />
+      <meta key="og-description" property="og:description" content={descriptionContent} data-rh="true" />
+      <meta key="og-locale" property="og:locale" content={localeName} data-rh="true" />
+      <meta key="og-site_name" property="og:site_name" content="Max." data-rh="true" />
+    </Head>
+  )
+}
+
+export default Seo
