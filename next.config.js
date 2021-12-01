5a5f3410ebd7dd0964daf55145f361002786b147
Maxmudov <qutbiddinmaxmudov@mail.ru>
2021-12-01 18:06:05 +0500

    initial config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  distDir: 'build',
+  i18n: {
+    locales: ['ru', 'en'],
+    defaultLocale: 'en',
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { IntlProvider } from "react-intl";
+import { useRouter } from "next/dist/client/router";
+import enLocale from "../locale/en.json";
+import ruLocale from "../locale/ru.json";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { locale = "en" } = useRouter();
+  return (
+    <IntlProvider
+      locale={locale}
+      messages={locale === "en" ? enLocale : ruLocale}
+    >
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
